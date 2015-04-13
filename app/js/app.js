ca55e59194d80a2f9f1eb8d17577cf332633b55a
David Asabina <david@supr.nu>
2015-04-13 12:07:43 +0200

    Setup bootstrapping w/ RequireJS

---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+requirejs.config({
+  baseUrl: '/bower_components',
+  shim: {
+    'angular': {
+      exports: 'angular',
+    },
+    'ngAnimate': {
+      deps: ['angular'],
+    },
+    'ngAria': {
+      deps: ['angular'],
+    },
+    'ngMaterial': {
+      deps: ['ngAnimate', 'ngAria'],
+    },
+    'd3': {
+      exports: 'd3',
+     },
+  },
+  paths: {
+    'app': '/app/js',
+    'angular': 'angular/angular',
+    'd3': 'd3/d3',
+    'ngAnimate': 'angular-animate/angular-animate',
+    'ngAria': 'angular-aria/angular-aria',
+    'ngMaterial': 'angular-material/angular-material',
+  },
+  deps: ['app/main'],
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,43 @@
-angular.module('d3', [])
-  .factory('d3Service', ['$document', '$q', '$rootScope', function($document, $q, $rootScope) {
-    console.log('d3 service factory prepping promise');
-    var d = $q.defer();
-    setTimeout(function() { console.log("resolved d3"); d.resolve(d3) }, 1000); // load d3 for real
-    return { 
-      dthree: function() { return d.promise; }
-    };
-  }
-]);
+'use strict';
+define(['angular', 'd3', 'ngMaterial'], function(ng, dthree, ngMaterial) {
+  var appName = 'OTSDesignTool';
 
-angular.module('OTSDesignTool', [ 'ngMaterial', 'd3' ]);
+  ng.module(appName, ['ngMaterial', 'd3']);
+  ng.element(document).ready(function() {
+    ng.bootstrap(document, ['OTSDesignTool']);
+    console.log('booted');
+  });
 
-angular.module('OTSDesignTool').directive('infraSchematic', ['d3Service', function(d3Service) { 
-  console.log("schematic here");
-  return {
-    restrict: 'E',
-    //require: '^ngModel',
-    scope: true,
-    link: function($scope, el, attr) {
-      console.log("link");
-      d3Service.dthree().then(function(dthree) { 
-        console.log("promised resolved");
-        console.log(dthree);
-        var svg = dthree.select(el[0]).append('svg');
-      });
-      console.log(el);
+  ng.module('d3', [])
+    .factory('d3Service', ['$document', '$q', '$rootScope', function($document, $q, $rootScope) {
+      console.log('d3 service factory prepping promise');
+      var d = $q.defer();
+      setTimeout(function() { console.log("resolved d3"); d.resolve(d3) }, 1000); // load d3 for real
+      return { 
+        dthree: function() { return d.promise; }
+      };
     }
-  }
-}]);
-
-angular.module('OTSDesignTool').controller("DemoCtrl", ['$scope', function($scope) {
-  console.log("keep them flowing");
-}] );
+  ]);
+  
+  ng.module(appName).directive('infraSchematic', ['d3Service', function(d3Service) { 
+    console.log("schematic here");
+    return {
+      restrict: 'E',
+      //require: '^ngModel',
+      scope: true,
+      link: function($scope, el, attr) {
+        console.log("link");
+        d3Service.dthree().then(function(dthree) { 
+          console.log("promised resolved");
+          console.log(dthree);
+          var svg = dthree.select(el[0]).append('svg');
+        });
+        console.log(el);
+      }
+    }
+  }]);
+  
+  ng.module(appName).controller("DemoCtrl", ['$scope', function($scope) {
+    console.log("keep them flowing");
+  }] );
+});
