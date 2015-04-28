ef53526ea7450474feae5c73c6be91c9ad7acb3c
David Asabina <david@supr.nu>
2015-04-28 16:59:40 +0200

    Added zoomable grid based on http://bl.ocks.org/mbostock/3892928

--- app/js/directives/infra-schematic.js
+++ app/js/directives/infra-schematic.js
@@ -18,9 +18,75 @@ define(['app/config', 'angular', 'app/lib/infrastructure_controller'], function(
           //scope.handler = InfraCtrl;
           console.log(scope.handler.getData());
 
-          var svg = undefined;
           d3Service.d3().then(function(d3) { 
-            var svg = d3.select(el[0]).append('svg');
+            function zoomed() {
+              svg.select(".x.axis").call(xAxis);
+              svg.select(".y.axis").call(yAxis);
+            }
+            
+            function reset() {
+              d3.transition().duration(750).tween("zoom", function() {
+                var ix = d3.interpolate(x.domain(), [-width / 2, width / 2]),
+                    iy = d3.interpolate(y.domain(), [-height / 2, height / 2]);
+                return function(t) {
+                  zoom.x(x.domain(ix(t))).y(y.domain(iy(t)));
+                  zoomed();
+                };
+              });
+            }
+
+            var getHeight = function() {
+              return d3.select(el[0]).node().getBoundingClientRect().height;
+            }
+            var getWidth = function() {
+              return d3.select(el[0]).node().getBoundingClientRect().width;
+            }
+
+            console.log("height is " + getHeight());
+            console.log("width is " + getWidth());
+            console.log("height is " + getHeight());
+
+            var x = d3.scale.linear()
+                .domain([-getWidth() / 2, getWidth() / 2])
+                .range([0, getWidth()]);
+            
+            var y = d3.scale.linear()
+                .domain([-getHeight() / 2, getHeight() / 2])
+                .range([getHeight(), 0]);
+
+            var margin = { top: 0, right: 0, bottom: 0, left: 0 };
+            var width = getWidth() - margin.left - margin.right;
+            var height = getHeight() - margin.top - margin.bottom;
+
+            var x = d3.scale.linear().domain([0, 100]).range([0, getWidth()]);
+            var y = d3.scale.linear().domain([0, 100]).range([0, getHeight()]);
+
+            var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5).tickSize(-height);
+            var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5).tickSize(-width);
+
+            var zoom = d3.behavior.zoom()
+                .x(x).y(y)
+                .scaleExtent([1, 10])
+                .on("zoom", zoomed);
+
+            console.log("getWidth is " + getWidth() + ", width is " + width);
+            console.log("getHeight is " + getHeight() + ", height is " + height);
+            var svg = d3.select(el[0]).append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(0,0)").call(zoom);
+
+            svg.append("rect")
+              .attr("width", width)
+              .attr("height", height);
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis);
   
             var mouseDownListener = function(d) {
               console.log('down');
@@ -31,9 +97,9 @@ define(['app/config', 'angular', 'app/lib/infrastructure_controller'], function(
               console.log('up');
               console.log(scope.handler);
               //console.log(d);
-              var circles = svg.selectAll("circle").data([5, 50]).enter().append("circle");
+              var circles = svg.selectAll("circle").data([1, 2, 4, 8, 16, 32, 64]).enter().append("circle");
               circles
-                .attr("cx", function(d, i) { return (i*50)+25; })
+                .attr("cx", function(d, i) { console.log("i is " + i + ", d is " + d); return (i*50)+25; })
                 .attr("cy", 30)
                 .attr("r", function(d) { return d });
             };
@@ -41,6 +107,8 @@ define(['app/config', 'angular', 'app/lib/infrastructure_controller'], function(
             svg
               .on('mousedown', mouseDownListener)
               .on('mouseup', mouseUpListener);
+
+            d3.select("button").on("click", reset);
           });
         },
       };
